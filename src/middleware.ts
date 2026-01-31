7b703e59445ea15c96cbd8938d8378a43a5c09af
bushrareen <naqeebrayeen717@gmail.com>
2026-01-31 15:50:49 +0530

    Final build fixed and api keys hidden

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,6 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // Is matcher se static files aur images exclude ho jati hain
+
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
